e31f09b27478c88a28558967b0a7f4d5a307c306
Marcelo Korin <167823583+NiroKz@users.noreply.github.com>
2025-08-31 18:35:54 -0300

    Delete SmartMealBackup10-06 directory

--- SmartMealBackup10-06/SmartMealSite/backend/routes/productionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerProduction } = require('../controllers/productionController');
-
-router.post('/', registerProduction);
-
-module.exports = router;
--- SmartMealBackup10-06/SmartMealSite/backend/routes/studentAccessRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getTodayStudentAccessData } = require("../controllers/studentAccessController");
-
-router.get("/today", getTodayStudentAccessData);
-
-module.exports = router;
