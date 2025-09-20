6c0c188f5dc3ca0ad6cedd2cd5b55e3fd1fc6f4a
NiroKz <marcelokorin14@gmail.com>
2025-09-20 16:25:27 -0300

    deleting backup

--- SmartMealBackup10-06/SmartMealSite/backend/routes/graphicRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const graphicController = require('../controllers/graphicController');
-
-router.get('/studentsCurses', graphicController.studentsCurses);
-
-module.exports = router;
--- SmartMealBackup10-06/SmartMealSite/backend/routes/liberationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleLiberation } = require('../controllers/liberationController');
-
-router.post('/', handleLiberation);
-
-module.exports = router;
